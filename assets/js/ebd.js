485a7ad0e59f11a75bc70f349b4732513cea23a8
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2015-01-24 12:36:23 -0500

    added doors

--- assets/js/ebd.js
+++ assets/js/ebd.js
@@ -1,10 +1,25 @@
 
-var width = $(document).width();
-var height = $(document).height();
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+
 
 $('gameArea').width(width);
 $('gameArea').height(height);
 var canvas = document.getElementById('gameArea');
+canvas.width = width;
+canvas.height = height;
+
 var context = canvas.getContext('2d');
 
 context.fillRect(0,0,width,height);
+
+context.strokeStyle="#FF0000";
+
+
+var startingPos = (width - 550)/2;
+var doors = [[startingPos,100,150,300], [startingPos+200,100,150,300], [startingPos+400,100,150,300]]
+
+for (var i = 0; i<doors.length; i++){
+    context.strokeRect(doors[i][0], doors[i][1], doors[i][2], doors[i][3]);
+}
