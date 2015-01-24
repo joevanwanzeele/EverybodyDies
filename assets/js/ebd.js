03d774c7f1796f3a4f39ea692f0b203e659e8aa5
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2015-01-24 13:53:23 -0500

    added door click stuff

--- assets/js/ebd.js
+++ assets/js/ebd.js
@@ -7,15 +7,21 @@ canvas.width = width;
 canvas.height = height;
 
 var context = canvas.getContext('2d');
+var doorWidth = 250;
+var doorHeight = 450;
+var doorSpacing = 70;
 
-var startingPos = (width - 550)/2;
-var doors = [[startingPos,100,150,300], [startingPos+200,100,150,300], [startingPos+400,100,150,300]]
+var startingPos = (width - ((doorWidth * 3) + (doorSpacing * 2)))/2;
+var doors = [[startingPos,100,doorWidth,doorHeight],
+  [startingPos + (doorWidth + doorSpacing),100,doorWidth,doorHeight],
+  [startingPos+ (doorWidth * 2 + doorSpacing * 2),100,doorWidth,doorHeight]];
 
 redrawDoors();
 
 
 
 $('#gameArea').mousemove(doorHoverHandler);
+$('#gameArea').click(doorClickHandler);
 
 function redrawDoors(){
   context.fillRect(0,0,width,height);
@@ -40,10 +46,34 @@ function doorHoverHandler(e){
 
     if (x >= xLeft && x <= xRight && y >= yTop && y <= yBottom){
       redrawDoors();
-      context.shadowBlur = 20;
+      context.shadowBlur = 10;
       context.shadowColor = "#FF0000";
 
       context.strokeRect(doors[i][0], doors[i][1], doors[i][2], doors[i][3]);
+    }
+  }
+}
+
+function doorClickHandler(e){
+  var x = e.pageX;
+  var y = e.pageY;
+
+  for (var i=0; i<doors.length; i++){
+    var xLeft = doors[i][0];
+    var xRight = doors[i][0] + doors[i][2];
+
+    var yTop = doors[i][1];
+    var yBottom = doors[i][1] + doors[i][3];
+
+    if (x >= xLeft && x <= xRight && y >= yTop && y <= yBottom){
+      //redrawDoors();
+      context.shadowBlur = 10;
+      context.shadowColor = "#FF0000";
+      var j = 0;
+      setInterval(function(){
+        context.strokeRect(doors[i][0], doors[i][1], doors[i][2], doors[i][3]);
+      },500);
+      return;
     }
   }
 }
