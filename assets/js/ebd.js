ba1ba311696b5485b8b67aa31dfb4a5dcab883c2
Joe VanWanzeele <joe.vanwanzeele@gmail.com>
2015-01-24 16:46:14 -0500

    stuff

--- assets/js/ebd.js
+++ assets/js/ebd.js
@@ -1,7 +1,6 @@
-var puzzles;
-
-
-
+var level = 0;
+var correctDoor = Math.floor(Math.random() * 2);
+var doorOpenAnimation;
 
 var width = window.innerWidth;
 var height = window.innerHeight;
@@ -9,7 +8,7 @@ var height = window.innerHeight;
 var canvas = document.getElementById('gameArea');
 canvas.width = width;
 canvas.height = height;
-var doorOpenAnimation;
+
 
 var context = canvas.getContext('2d');
 var doorWidth = 250;
@@ -23,14 +22,12 @@ var doors = [[startingPos,100,doorWidth,doorHeight],
 
 redrawDoors();
 
-
-
 $('#gameArea').mousemove(doorHoverHandler);
 $('#gameArea').click(doorClickHandler);
 
 function redrawDoors(doorIndex){
   context.fillRect(0,0,width,height);
-
+  context.save();
   context.strokeStyle="#FF0000";
 
   if (doorIndex != null){
@@ -39,10 +36,11 @@ function redrawDoors(doorIndex){
   else{
     for (var i = 0; i<doors.length; i++){
         context.strokeRect(doors[i][0], doors[i][1], doors[i][2], doors[i][3]);
-        var fillSequence = ['#ffff00', '#ffffff', '#00ff00', '#0000ff', '#00ff00', '#0000ff', '#00ff00', '#0000ff'];
-        drawPuzzle(doors[i][0]+(doorWidth/2), doors[i][1]+(doorHeight/2), doorWidth/2, doorHeight/2, fillSequence);
+        var pattern = i == correctDoor ? puzzles[level][1] : puzzles[level][0];
+        drawPuzzle(doors[i][0]+(doorWidth/2), doors[i][1]+(doorHeight/2), doorWidth/2, doorHeight/2, pattern);
     }
   }
+  context.restore();
 }
 
 function doorHoverHandler(e){
@@ -59,10 +57,12 @@ function doorHoverHandler(e){
     if (x >= xLeft && x <= xRight && y >= yTop && y <= yBottom){
       clearInterval(doorOpenAnimation);
       redrawDoors();
+      canvas.save();
       context.shadowBlur = 10;
       context.shadowColor = "#FF0000";
 
       context.strokeRect(doors[i][0], doors[i][1], doors[i][2], doors[i][3]);
+      canvas.restore();
     }
   }
 }
@@ -80,14 +80,17 @@ function doorClickHandler(e){
     var yBottom = doors[i][1] + doors[i][3];
 
     if (x >= xLeft && x <= xRight && y >= yTop && y <= yBottom){
-      redrawDoors(i);
       context.shadowBlur = 10;
-      context.shadowColor = "#FF0000";
+      context.shadowColor = correctDoor == i ? "#0000FF" : "#FF0000";
+      context.strokeStyle = correctDoor == i ? "#0000FF" : "#FF0000";
       var j = 1;
+      redrawDoors(i);
       doorOpenAnimation = setInterval(function(){
         j = j*1.1;
         context.strokeRect(doors[i][0]-j, doors[i][1]-j, doors[i][2]+j*2, doors[i][3]+j*2);
       },.5);
+
+      correctDoor = Math.floor(Math.random() * 2);
       return;
     }
   }
@@ -95,7 +98,7 @@ function doorClickHandler(e){
 
 function drawPuzzle(cx, cy, width, height, fillSequence){
   //context.clearRect(x,y,width,height);
-
+  context.save();
   var nbr_circles = fillSequence.length;
 
   var lg_rad = (width/2) * .85;
@@ -103,10 +106,6 @@ function drawPuzzle(cx, cy, width, height, fillSequence){
   var sm_rad = (lg_circ / nbr_circles) / 2;
 
   for (var i = 1; i <= nbr_circles; ++i) {
-    var originalss = context.strokeStyle;
-    var originalShadowBlur = context.shadowBlur;
-    var originalShadowColor = context.shadowColor;
-
     context.strokeStyle = '#000';
     context.shadowBlur = 0;
     context.shadowColor = "#000";
@@ -118,10 +117,6 @@ function drawPuzzle(cx, cy, width, height, fillSequence){
     var y = cy + Math.sin(angle) * lg_rad;
     context.arc(x, y, sm_rad, 0, 2*Math.PI, false);
     context.fill();
-
-    context.strokeStyle = originalss;
-    context.shadowBlur = originalShadowBlur;
-    context.shadowColor = originalShadowColor;
   }
-  context.fillStyle='#000';
+  context.restore();
 }
